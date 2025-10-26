c42fc1b2bebd40aa29c6ada294240a554a83d22e
Borodin Gregory <grihabor@gmail.com>
2025-10-26 15:25:01 +0100

    Add the script

---
+++ README.md
@@ -0,0 +1,13 @@
+## transcribe
+
+### Install
+
+```bash
+uv tool install transcribe@git+git@github.com:grihabor/transcribe.git
+```
+
+### Run
+
+```bash
+transcribe speech.mp3
+```
---
+++ transcribe.py
@@ -0,0 +1,36 @@
+import re
+import argparse
+import textwrap
+from pathlib import Path
+
+import whisper
+
+
+def _print(text):
+    for line in textwrap.wrap(text):
+        print(line)
+    print()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--split-by", default="Speaker")
+    parser.add_argument("path")
+    args = parser.parse_args()
+    model = whisper.load_model("small")
+
+    path_txt = Path(f"{args.path}.txt")
+    if not path_txt.exists():
+        result = model.transcribe(path_txt)
+        path_txt.write_text(result["text"])
+
+    text = path_txt.read_text()
+    matches = list(re.compile(args.split_by).finditer(text))
+
+    for a, b in zip(matches[:-1], matches[1:], strict=False):
+        _print(text[a.span()[0] : b.span()[0]])
+    _print(text[matches[-1].span()[0] :])
+
+
+if __name__ == "__main__":
+    main()
